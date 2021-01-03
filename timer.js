841da276088ec8c92479ae111bab1ec9af593af0
misz4 <76251380+misz4@users.noreply.github.com>
2021-01-03 14:11:12 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -6,6 +6,6 @@ function timer(){
     if(minute<10) minute = "0"+minute
     var second = today.getSeconds();
     if(second<10) second = "0"+second
-    document.getElementById("zegar").innerHTML = hour+":"+minute+":"+second;
+    document.getElementById("").innerHTML = hour+":"+minute+":"+second;
     setTimeout("timer()", 1000);
     }
